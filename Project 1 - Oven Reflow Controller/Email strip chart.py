b33b6d08437b84669a8bb5490f0340e64dedd327
DESKTOP-I803JUI\Kay Xi <kayxi2014@gmail.com>
2017-02-15 10:20:17 -0800

    everything added. colored lines worked. csv under process

--- Project 1 - Oven Reflow Controller/Email strip chart.py
+++ Project 1 - Oven Reflow Controller/Email strip chart.py
@@ -43,7 +43,7 @@ from twilio.rest import TwilioRestClient
 import time
 
 ser = serial.Serial(
-    port='COM4',
+    port='COM9',
     baudrate=115200,
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_TWO,
