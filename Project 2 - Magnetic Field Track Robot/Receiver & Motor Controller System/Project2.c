3fd378b50abec9fadfe6de67fc7fb2ba72d2257c
Daniel Zhou <danielzhou4970@gmail.com>
2017-03-28 15:56:07 -0700

    Removed an unecessary function. Still testing base movement

--- Project 2 - Magnetic Field Track Robot/Receiver & Motor Controller System/Project2.c
+++ Project 2 - Magnetic Field Track Robot/Receiver & Motor Controller System/Project2.c
@@ -43,17 +43,6 @@ void UART2Configure(int baud_rate)
     U2MODESET = 0x8000;     // enable UART2
 }
 
-// Good information about ADC in PIC32 found here:
-// http://umassamherstm5.org/tech-tutorials/pic32-tutorials/pic32mx220-tutorials/adc
-void ADCConf(void)
-{
-    AD1CON1CLR = 0x8000;    // disable ADC before configuration
-    AD1CON1 = 0x00E0;       // internal counter ends sampling and starts conversion (auto-convert), manual sample
-    AD1CON2 = 0;            // AD1CON2<15:13> set voltage reference to pins AVSS/AVDD
-    AD1CON3 = 0x0f01;       // TAD = 4*TPB, acquisition time = 15*TAD 
-    AD1CON1SET=0x8000;      // Enable ADC
-}
-
 // Interrupt Service Routine for Timer2 which has Interrupt Vector 8 and initalized with priority level 3
 void __ISR(_TIMER_2_VECTOR, IPL7AUTO) Timer2_ISR(void)
 {
